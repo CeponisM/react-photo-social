9e58b662828530b2eafca31f99086cae7e9bafeb
CeponisM <Mantasceponis5@gmail.com>
2023-09-18 15:59:00 -0500

    interface styling, bug fixes

--- src/components/newPost/NewPost.js
+++ src/components/newPost/NewPost.js
@@ -12,6 +12,7 @@ function NewPost({ onNewPost }) {
   const [uploading, setUploading] = useState(false);
   const [uploadError, setUploadError] = useState(null);
   const [editing, setEditing] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const imageEditorRef = useRef(null); // Initialize as null
 
@@ -19,12 +20,12 @@ function NewPost({ onNewPost }) {
     setImageFile(file);
     setImageUrl(URL.createObjectURL(file));
     setEditing(true); // Start editing when an image is selected
+    setShowModal(true); // Show the modal
   };
 
   const handleImageEdit = () => {
-    if (imageEditorRef.current) { // Check if the ref is defined
+    if (imageEditorRef.current) {
       const editedImageBlob = imageEditorRef.current.getInstance().toDataURL();
-      // Convert the data URL to a Blob
       const byteString = atob(editedImageBlob.split(',')[1]);
       const ab = new ArrayBuffer(byteString.length);
       const ia = new Uint8Array(ab);
@@ -37,6 +38,14 @@ function NewPost({ onNewPost }) {
       setImageUrl(URL.createObjectURL(editedImageBlobObject));
       setEditing(false); // Finish editing
     }
+  };
+
+  const handleCancelUpload = () => {
+    // Clear image data, cancel upload, and close the modal
+    setImageFile(null);
+    setImageUrl(null);
+    setEditing(false);
+    setShowModal(false);
   };
 
   const handleSubmit = async (e) => {
@@ -58,6 +67,7 @@ function NewPost({ onNewPost }) {
         setCaption('');
         setImageUrl(null);
         setImageFile(null);
+        setShowModal(false); // Close the modal on successful upload
       } catch (error) {
         console.error('Error creating post:', error);
         setUploadError('Error creating post. Please try again.');
@@ -71,69 +81,79 @@ function NewPost({ onNewPost }) {
   };
 
   return (
-    <div className="new-post-container">
-      <h2 className="new-post-title">Create a New Post</h2>
-      <form className="new-post-form" onSubmit={handleSubmit}>
-        <textarea
-          className="new-post-textarea"
-          placeholder="Write a caption..."
-          value={caption}
-          onChange={(e) => setCaption(e.target.value)}
-        />
-        {/* Pass the handleImageSelect function as onImageUpload */}
-        <ImageUploader onImageUpload={handleImageSelect} />
-        {editing ? (
-          <div className="image-editor-container">
-            {/* Render the image editor while editing */}
-            <ImageEditor
-              ref={imageEditorRef}
-              includeUI={{
-                loadImage: {
-                  path: imageUrl,
-                  name: 'SampleImage',
-                },
-                menu: [
-                  'crop',
-                  'flip',
-                  'rotate',
-                  'draw',
-                  'shape',
-                  'icon',
-                  'text',
-                  'mask',
-                  'filter',
-                ],
-                initMenu: '',
-                uiSize: {
-                  width: '100%',
-                  height: '700px',
-                },
-                menuBarPosition: 'bottom',
-              }}
-              cssMaxHeight={500}
-              cssMaxWidth={700}
-              selectionStyle={{
-                cornerSize: 20,
-                rotatingPointOffset: 70,
-              }}
-              usageStatistics={true}
-            />
-            <button onClick={handleImageEdit}>Done Editing</button>
-          </div>
-        ) : (
-          // Conditionally render the "Post" button
-          imageFile ? (
-            <button
-              className="new-post-image-upload-button"
-              type="submit"
-              disabled={uploading}
-            >
-              {uploading ? 'Uploading...' : 'Post'}
-            </button>
-          ) : null
-        )}
-        {uploadError && <p className="error">{uploadError}</p>}
-      </form>
+    <div className={`new-post-container ${editing ? 'editing' : ''}`}>
+      {showModal && (
+        <div className="overlay" onClick={handleCancelUpload}></div>
+      )}
+      <div className={`upload-modal ${showModal ? 'show' : ''}`}>
+        <h2 className="new-post-title">Create a New Post</h2>
+        <form className="new-post-form" onSubmit={handleSubmit}>
+          <textarea
+            className="new-post-textarea"
+            placeholder="Write a caption..."
+            value={caption}
+            onChange={(e) => setCaption(e.target.value)}
+          />
+          {/* Pass the handleImageSelect function as onImageUpload */}
+          <ImageUploader onImageUpload={handleImageSelect} />
+          {editing ? (
+            <div className="image-editor-container">
+              {/* Render the image editor while editing */}
+              <ImageEditor
+                ref={imageEditorRef}
+                includeUI={{
+                  loadImage: {
+                    path: imageUrl,
+                    name: 'SampleImage',
+                  },
+                  menu: [
+                    'crop',
+                    'flip',
+                    'rotate',
+                    'draw',
+                    'shape',
+                    'icon',
+                    'text',
+                    'mask',
+                    'filter',
+                  ],
+                  initMenu: '',
+                  uiSize: {
+                    width: '100%',
+                    height: '700px',
+                  },
+                  menuBarPosition: 'bottom',
+                }}
+                cssMaxHeight={500}
+                cssMaxWidth={700}
+                selectionStyle={{
+                  cornerSize: 20,
+                  rotatingPointOffset: 70,
+                }}
+                usageStatistics={true}
+              />
+              <button onClick={handleImageEdit}>Done Editing</button>
+            </div>
+          ) : (
+            // Conditionally render the "Post" button
+            imageFile ? (
+              <div className="upload-actions">
+                <button
+                  className="new-post-image-upload-button"
+                  type="submit"
+                  disabled={uploading}
+                >
+                  {uploading ? 'Uploading...' : 'Post'}
+                </button>
+                <button className="cancel-button" onClick={handleCancelUpload}>
+                  Cancel
+                </button>
+              </div>
+            ) : null
+          )}
+          {uploadError && <p className="error">{uploadError}</p>}
+        </form>
+      </div>
     </div>
   );
 }
